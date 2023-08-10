71041edbfdd888a78df87a45ecd8541763426413
JustFog36 <jfogle36@gmail.com>
2023-08-09 20:46:09 -0400

    added the map to the page

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,33 @@
+var searchButton = document.getElementById("searchButton");
+var displayResults = document.querySelector(".map > div:last-child");
+
 searchButton.addEventListener("click", function() {
-    var zipCodeInput = document.getElementById("zipCode")
+    var zipCodeInput = document.getElementById("zipCode");
     var zipCode = zipCodeInput.value;
 
-    console.log("User entered zipe code:", zipCode)
-})
+    console.log("User entered zipe code:", zipCode);
+
+});
+
+
+
+function initMap() {
+    const myLatLng = {
+      lat: 27.98325538635254,
+      lng: -81.69874572753906
+    };
+    const map = new google.maps.Map(document.getElementById("gmp-map"), {
+      zoom: 13,
+      center: myLatLng,
+      fullscreenControl: false,
+      zoomControl: true,
+      streetViewControl: false
+    });
+    new google.maps.Marker({
+      position: myLatLng,
+      map,
+      title: "My location"
+    });
+  }
+
+    
